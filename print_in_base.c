d46018f27650c9156d0fffa96014e5ea819238fa
Nonna <nonna.arzumanyan.111@gmail.com>
2025-01-24 11:53:10 +0400

    print in base
    recursive function that takes a number, any base, and prints the representation of that number in the base system.

---
+++ print_in_base.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void print_in_base(int num, int base) {
+    if (num == 0) {
+        return;  
+    } else {
+        print_in_base(num / base, base); 
+        printf("%d", num % base);  
+    }
+}
+
+int main() {
+    int num = 0;
+    printf("Please enter the number: ");
+    scanf("%d", &num);
+    
+    int base = 0;
+    printf("Please enter the base: ");
+    scanf("%d", &base);
+    
+    if (num == 0) {
+        printf("0");
+    } else {
+        printf("The number in base %d is: ", base);
+        print_in_base(num, base);
+    }  
+    printf("\n");
+
+    return 0;
+}
